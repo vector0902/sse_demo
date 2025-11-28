23c4c6696d4e5a52957d987949e864b202fb4cf9
rootuc <rootuc@notunixchan>
2025-11-28 15:24:39 +0800

    init

---
+++ src/test/java/com/example/sse_demo/SseDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sse_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SseDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
